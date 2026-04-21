c4317158280ddcada840df1e08266edeac66106b
krishnakala987-byte <krishnakala987@gmail.com>
2026-04-21 18:38:13 +0530

    Use data source for dynamic AMI retrieval

--- Day-1/main.tf
+++ Day-1/main.tf
@@ -2,8 +2,24 @@ provider "aws" {
   region = "us-east-1"
 }
 
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  owners = ["099720109477"]
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
+
 resource "aws_instance" "my_server" {
-  ami           = "ami-0ec10929233384c7f" # Ubuntu (N. Virginia)
+  ami           = data.aws_ami.ubuntu.id
   instance_type = "t2.micro"
 
   tags = {
