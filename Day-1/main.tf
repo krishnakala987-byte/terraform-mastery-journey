f3a7fa2ed784477ad9861de901bef09de6daad9e
Krishna Kala <your-email@gmail.com>
2026-04-21 14:33:45 +0000

    Added security group and SSH config

--- Day-1/main.tf
+++ Day-1/main.tf
@@ -2,10 +2,11 @@ provider "aws" {
   region = "us-east-1"
 }
 
+# Get latest Ubuntu AMI
 data "aws_ami" "ubuntu" {
   most_recent = true
 
-  owners = ["099720109477"]
+  owners = ["099720109477"] # Canonical
 
   filter {
     name   = "name"
@@ -18,11 +19,35 @@ data "aws_ami" "ubuntu" {
   }
 }
 
+# Security Group (Allow SSH)
+resource "aws_security_group" "my_sg" {
+  name        = "terraform-sg"
+  description = "Allow SSH access"
+
+  ingress {
+    description = "SSH access"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]  # open for learning
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+# EC2 Instance
 resource "aws_instance" "my_server" {
-  ami           = data.aws_ami.ubuntu.id
-  instance_type = "t2.micro"
+  ami                    = data.aws_ami.ubuntu.id
+  instance_type          = "t2.micro"
+  key_name               = "ansible-demo"
+  vpc_security_group_ids = [aws_security_group.my_sg.id]
 
   tags = {
-    Name = "MyFirstTerraformServer"
+    Name = "Terraform-Ubuntu-Server"
   }
 }
