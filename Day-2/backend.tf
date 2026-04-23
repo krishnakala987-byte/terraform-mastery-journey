dafb02952398d9ae33f017723a1fd2e10a714a72
krishnakala987-byte <krishnakala987@gmail.com>
2026-04-23 11:08:48 +0530

    Update backend.tf

--- Day-2/backend.tf
+++ Day-2/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket         = "terraform-state-krishna-123"
-    key            = "terraform.tfstate"
+    key            = "env/${terraform.workspace}/terraform.tfstate"
     region         = "us-east-1"
     dynamodb_table = "terraform-lock"
   }
