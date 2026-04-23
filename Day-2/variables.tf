754be91906d2d358ba66cf444fb99e5ac797e6a7
krishnakala987-byte <krishnakala987@gmail.com>
2026-04-23 11:12:07 +0530

    Update variables.tf

--- Day-2/variables.tf
+++ Day-2/variables.tf
@@ -1,4 +1,29 @@
-variable "region" {}
-variable "instance_type" {}
-variable "env" {}
-variable "ami" {}
+variable "region" {
+  description = "AWS region"
+  type        = string
+}
+
+variable "instance_type" {
+  description = "Instance type for prod"
+  type        = string
+}
+
+variable "env" {
+  description = "Environment (dev/prod)"
+  type        = string
+}
+
+variable "ami" {
+  description = "AMI ID"
+  type        = string
+}
+
+variable "allowed_ssh_cidr" {
+  description = "Allowed IP for SSH"
+  type        = string
+}
+
+variable "bucket_name" {
+  description = "S3 bucket name"
+  type        = string
+}
