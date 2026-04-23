59ce4f94e32b402822ec409b6ea70dae106de5b7
krishnakala987-byte <krishnakala987@gmail.com>
2026-04-23 13:38:01 +0530

    Update main.tf

--- Day-2/main.tf
+++ Day-2/main.tf
@@ -33,9 +33,9 @@ resource "aws_instance" "server" {
   ami = var.ami
 
   instance_type = var.env == "prod" ? var.instance_type : "t2.micro"
-
+  
   vpc_security_group_ids = [aws_security_group.web_sg.id]
-
+  key_name = "terraform-key"
   user_data = <<-EOF
               #!/bin/bash
               sudo apt update -y
