5468ba62146280c91f0b2fcacd05bf92e27bb838
krishnakala987-byte <krishnakala987@gmail.com>
2026-04-23 10:16:23 +0530

    Create backend.tf

---
+++ Day-2/backend.tf
@@ -0,0 +1,8 @@
+terraform {
+  backend "s3" {
+    bucket         = "terraform-state-krishna-123"
+    key            = "env/terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "terraform-lock"
+  }
+}
