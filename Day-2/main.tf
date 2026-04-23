460fa70ca9d1eb83386434b8651fa472d25a5cef
krishnakala987-byte <krishnakala987@gmail.com>
2026-04-23 11:13:53 +0530

    Add security group and update EC2 instance configuration

--- Day-2/main.tf
+++ Day-2/main.tf
@@ -1,9 +1,59 @@
+# Security Group
+resource "aws_security_group" "web_sg" {
+  name = "${var.env}-sg"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = [var.allowed_ssh_cidr]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "${var.env}-sg"
+  }
+}
+
+#  EC2 Instance
 resource "aws_instance" "server" {
-  ami           = var.ami
+  ami = var.ami
 
   instance_type = var.env == "prod" ? var.instance_type : "t2.micro"
 
+  vpc_security_group_ids = [aws_security_group.web_sg.id]
+
+  user_data = <<-EOF
+              #!/bin/bash
+              sudo apt update -y
+              sudo apt install nginx -y
+              systemctl start nginx
+              systemctl enable nginx
+              EOF
+
   tags = {
     Name = "${var.env}-server"
+  }
+}
+
+#  S3 Bucket
+resource "aws_s3_bucket" "bucket" {
+  bucket = var.bucket_name
+
+  tags = {
+    Name = "${var.env}-bucket"
   }
 }
