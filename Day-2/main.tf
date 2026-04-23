520286fcdcbd4aac06b0405d64f3e9eb1690618f
krishnakala987-byte <krishnakala987@gmail.com>
2026-04-23 13:55:16 +0530

    Update main.tf

--- Day-2/main.tf
+++ Day-2/main.tf
@@ -1,17 +1,18 @@
-# Security Group
 resource "aws_security_group" "web_sg" {
   name = "${var.env}-sg"
 
   ingress {
-    from_port   = 22
-    to_port     = 22
+    description = "HTTP"
+    from_port   = 80
+    to_port     = 80
     protocol    = "tcp"
-    cidr_blocks = [var.allowed_ssh_cidr]
+    cidr_blocks = ["0.0.0.0/0"]
   }
 
   ingress {
-    from_port   = 80
-    to_port     = 80
+    description = "SSH"
+    from_port   = 22
+    to_port     = 22
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
@@ -22,26 +23,21 @@ resource "aws_security_group" "web_sg" {
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
-
-  tags = {
-    Name = "${var.env}-sg"
-  }
 }
 
-#  EC2 Instance
 resource "aws_instance" "server" {
-  ami = var.ami
-
+  ami           = var.ami
   instance_type = var.env == "prod" ? var.instance_type : "t2.micro"
-  
+
   vpc_security_group_ids = [aws_security_group.web_sg.id]
-  key_name = "ansible-demo"
+
   user_data = <<-EOF
               #!/bin/bash
-              sudo apt update -y
-              sudo apt install nginx -y
+              apt update -y
+              apt install nginx -y
               systemctl start nginx
               systemctl enable nginx
+              echo "<h1>${var.env} environment - Terraform Project</h1>" > /var/www/html/index.html
               EOF
 
   tags = {
@@ -49,11 +45,6 @@ resource "aws_instance" "server" {
   }
 }
 
-#  S3 Bucket
-resource "aws_s3_bucket" "bucket" {
-  bucket = var.bucket_name
-
-  tags = {
-    Name = "${var.env}-bucket"
-  }
+resource "aws_eip" "elastic_ip" {
+  instance = aws_instance.server.id
 }
