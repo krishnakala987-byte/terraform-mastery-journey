9ecae0d57c77eda39fb20856972fc926914825ce
krishnakala987-byte <krishnakala987@gmail.com>
2026-04-24 12:02:55 +0530

    Update main.tf

--- Day-3/modules/security_group/main.tf
+++ Day-3/modules/security_group/main.tf
@@ -1,7 +1,10 @@
 resource "aws_security_group" "web_sg" {
   name = "${var.env}-sg"
 
+  description = "Security group for ${var.env} environment"
+
   ingress {
+    description = "Allow HTTP"
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
@@ -9,6 +12,7 @@ resource "aws_security_group" "web_sg" {
   }
 
   ingress {
+    description = "Allow SSH (restricted)"
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
@@ -16,9 +20,15 @@ resource "aws_security_group" "web_sg" {
   }
 
   egress {
+    description = "Allow all outbound traffic"
     from_port   = 0
     to_port     = 0
     protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name        = "${var.env}-sg"
+    Environment = var.env
   }
 }
