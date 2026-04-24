004b91bc9510f627563602335c5581ec05c7a0a9
krishnakala987-byte <krishnakala987@gmail.com>
2026-04-24 11:58:01 +0530

    Add outputs for EC2 instance ID and public IP
    Added output for EC2 instance ID and updated public IP output.

--- Day-3/modules/ec2/outputs.tf
+++ Day-3/modules/ec2/outputs.tf
@@ -1,3 +1,9 @@
 output "public_ip" {
-  value = aws_instance.server.public_ip
+  description = "Public IP of EC2 instance"
+  value       = aws_eip.elastic_ip.public_ip
+}
+
+output "instance_id" {
+  description = "EC2 Instance ID"
+  value       = aws_instance.server.id
 }
