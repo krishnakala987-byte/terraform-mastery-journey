a69188ac38849b2c9be53c5e99c72da95ac91e2c
krishnakala987-byte <krishnakala987@gmail.com>
2026-04-24 11:51:41 +0530

    Update variables.tf

--- Day-3/modules/ec2/variables.tf
+++ Day-3/modules/ec2/variables.tf
@@ -1,4 +1,19 @@
-variable "ami" {}
-variable "instance_type" {}
-variable "env" {}
-variable "sg_id" {}
+variable "ami" {
+  description = "AMI ID"
+  type        = string
+}
+
+variable "instance_type" {
+  description = "Instance type"
+  type        = string
+}
+
+variable "env" {
+  description = "Environment (dev/prod)"
+  type        = string
+}
+
+variable "sg_id" {
+  description = "Security Group ID"
+  type        = string
+}
