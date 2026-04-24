c08f34335736f6e5cd9b62e549d95917327b4dd9
krishnakala987-byte <krishnakala987@gmail.com>
2026-04-24 12:00:04 +0530

    Update variables.tf

--- Day-3/modules/s3/variables.tf
+++ Day-3/modules/s3/variables.tf
@@ -1,1 +1,9 @@
-variable "bucket_name" {}
+variable "bucket_name" {
+  description = "Name of the S3 bucket"
+  type        = string
+}
+
+variable "env" {
+  description = "Environment (dev/prod)"
+  type        = string
+}
