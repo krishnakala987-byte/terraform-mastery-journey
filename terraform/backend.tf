23200a63d6263aa8d449cde704732421b8bb66b8
krishnakala987-byte <krishnakala987@gmail.com>
2026-05-05 18:39:54 +0530

    Delete terraform directory

--- terraform/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "terraform-state-bucket-day-8"
-    key            = "day8/terraform.tfstate"
-    region         = "us-east-1"
-    use_lockfile   = true
-  }
-}
