7a9f9782284b020e6b93100e44d99f5dddea4807
Krishna Kala <your-email@gmail.com>
2026-05-05 04:12:31 +0000

    Test again

--- Day-8/main.tf
+++ Day-8/main.tf
@@ -4,7 +4,7 @@ provider "aws" {
 
 resource "aws_instance" "my_ec2" {
   ami           = "ami-091138d0f0d41ff90"
-  instance_type = "t2.small"
+  instance_type = "t2.micro"
 
   tags = {
     Name = "terraform project"
