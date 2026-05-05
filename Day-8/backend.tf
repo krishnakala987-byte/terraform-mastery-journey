4e0646636afe089cbdeb3e02b927e64d67b86e3b
krishnakala987-byte <krishnakala987@gmail.com>
2026-05-05 07:05:08 +0530

    Delete Day-8 directory

--- Day-8/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "tf-state-krishna-demo"
-    key            = "ec2/terraform.tfstate"
-    region         = "us-east-1"
-    dynamodb_table = "tf-lock-table" 
-  }
-}
