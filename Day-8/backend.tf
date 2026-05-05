f6b8659cd594267a103464e36b9411d52f9a4b81
Krishna Kala <your-email@gmail.com>
2026-05-05 14:08:25 +0000

    updated everything

--- Day-8/backend.tf
+++ Day-8/backend.tf
@@ -1,7 +1,9 @@
 terraform {
   backend "s3" {
-    bucket = "terraform-state-bucket-day-8"
-    key    = "day-8/terraform.tfstate"
-    region = "us-east-1"
+    bucket         = "terraform-state-bucket-day-8"
+    key            = "day-8/terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "terraform-lock-table"
+    encrypt        = true
   }
 }
